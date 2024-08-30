fab500b2417583a39b1b3a68dcd0e19293de74bb
Ben Costolo <ben.costolo@gmail.com>
2024-08-30 19:06:39 -0400

    wip

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,14 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import UnoCSS from "unocss/astro";
 
+console.log(import.meta.env)
+console.log(process.env)
+
 export default defineConfig({
   site:
-    process.env.VERCEL_ENV === "production"
-      ? "https://benco.st"
-      : "https://localhost:3000",
+    process.env.NODE_ENV === "production"
+      ? process.env.SITE_URL
+      : process.env.LOCAL_URL,
   trailingSlash: "ignore",
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
